9d07fc31972ed5a6bdc14306336907e811775994
Narangerel0121 <nrngrl0121@gmail.com>
2025-07-01 10:32:55 +0800

    serveral things need to be fixed

--- front/src/app/user/(home)/hobby/[id]/components/SuccessDialog.tsx
+++ front/src/app/user/(home)/hobby/[id]/components/SuccessDialog.tsx
@@ -17,10 +17,20 @@ export default function SuccessDialog({ open, onOpenChange }: SuccessDialogProps
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="w-[556px] h-[459px] bg-[#DAD8F6]">
         <DialogHeader>
-          <DialogTitle className="text-center text-xl text-slate-700 font-normal mt-15">Та мөс хагалах анхны алхмаа амжилттай хийлээ. <br></br>Уулзалтанд тань амжилт хүсье</DialogTitle>
+          <DialogTitle className="relative">
+            <div>
+              <div className="text-center text-xl text-slate-700 font-normal mt-15">
+                Та мөс хагалах анхны алхмаа амжилттай хийлээ.
+              </div>
+              <p className="text-center text-lg text-slate-700 font-normal">Уулзалтанд тань амжилт хүсье</p>
+            </div>
+            <div className="absolute inset-x-0 top-11 flex justify-between w-full">
+              <img src="/star.svg" className="w-[23px] h-[28px]" />
+              <img src="/star.svg" className="w-[23px] h-[28px]" />
+            </div>
+          </DialogTitle>
           <DialogDescription className="text-center">
-            {/* <Image src={`/alertpinguin.png`} alt="image" width={320} height={320} className="absolute bottom-0 self-center"></Image> */}
-            <Image src={`/alertpinguin.png`} alt="image" width={320} height={320} style={{objectFit: "fill"}} className="absolute inset-x-0 bottom-0 mx-auto"></Image>
+            <Image src={`/alertpinguin.png`} alt="image" width={320} height={320} style={{ objectFit: "fill" }} className="absolute inset-x-0 bottom-0 mx-auto"></Image>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
--- front/src/app/user/(home)/request/page.tsx
+++ front/src/app/user/(home)/request/page.tsx
@@ -6,23 +6,23 @@ import { ResBody } from "./components/Res";
 export default function RequestPage() {
   return (
     <div className="w-full h-screen px-5">
-      <div className="text-center pb-[60px]">
-        <h1 className="text-slate-700 text-2xl leading-8 font-semibold">
+      <div className="text-center p-4 mb-14">
+        <h1 className="text-slate-800 text-3xl leading-8 font-medium">
           Танд ирсэн мэдэгдлүүд
         </h1>
       </div>
       <div className="flex items-center ">
-        <Tabs defaultValue="res" className="w-full ">
-          <TabsList className="w-full bg-slate-50 rounded-[6px] mb-8 p-2">
+        <Tabs defaultValue="res" className="w-full h-15 p-2">
+          <TabsList className="w-full bg-slate-50 py-3 mb-8">
             <TabsTrigger
               value="res"
-              className="data-[state=active]:bg-[#E5EFF8] data-[state=active]:text-slate-800 data-[state=active]:font-semibold text-slate-500 text-base px-8 py-2 rounded-[6px] transition-colors h-[44px]"
+              className="data-[state=active]:bg-[#E5EFF8] data-[state=active]:text-slate-800 data-[state=active]:font-medium data-[state=active]:py-3 data-[state=active]:rounded-[8px_0px_0px_8px] rounded-[8px_0px_0px_8px] text-slate-400 text-sm py-3 font-medium "
             >
               Ирсэн хүсэлтүүд
             </TabsTrigger>
             <TabsTrigger
               value="req"
-              className="data-[state=active]:bg-[#E5EFF8] data-[state=active]:text-slate-800 data-[state=active]:font-semibold text-slate-500 text-base px-8 py-2 rounded-[6px] transition-colors h-[44px]"
+              className="data-[state=active]:bg-[#E5EFF8] data-[state=active]:text-slate-800 data-[state=active]:font-medium data-[state=active]:py-3 data-[state=active]:rounded-[0px_8px_8px_0px] rounded-[0px_8px_8px_0px] text-slate-400 text-sm py-3 font-medium"
             >
               Илгээсэн хүсэлтүүд
             </TabsTrigger>
