c73a338cd60e27885d386e10dd46e8c0f2d9330e
24LP1447 24LP1447 <24LP1447@Administrators-iMac-2.local>
2025-07-01 16:28:13 +0800

    home page fully done

--- front/src/app/user/(home)/page.tsx
+++ front/src/app/user/(home)/page.tsx
@@ -12,7 +12,7 @@
 
     return (
     <div className="flex items-center justify-center h-screen">
-      <motion.div initial={{opacity:0 , y:-100}}  animate={{ opacity: 1, y: 0 }}  transition={{ duration: 0.5 }}   className="w-[1090px] h-[742px] bg-[#eaf2fc] rounded-[24px] flex justify-around items-center ">
+      <motion.div initial={{opacity:0 , y:-100}}  animate={{ opacity: 1, y: 0 }}  transition={{ duration: 0.5 }}   className="w-[1090px] h-[742px] bg-amber-50 rounded-[24px] flex justify-around items-center ">
         <div>      
             <motion.div   className="relative z-20 flex flex-col gap-[40px]  justify-center items-start">
             <motion.h1 initial={{opacity:0 , x:-100}}  animate={{ opacity: 1, x: 0 }}  transition={{ duration: 0.5  ,delay:0.3}} className="font-medium  w-[481px] h-[40px] text-[36px]  text-gray-800 leading-tight 
@@ -32,7 +32,7 @@
     damping: 10 ,
     mass: 1
   }}
-  className="w-[481px] h-12 bg-blue-100 rounded-md text-blue-600 text-lg font-medium hover:bg-blue-200 transition"
+  className="w-[481px] h-[40px] bg-amber-100 rounded-[6px] text-amber-900  text-sm font-medium hover:bg-amber-200 transition"
   onClick={handleStartClick}
 >
   Мөс хагалъя 
@@ -41,7 +41,7 @@
           </div>
         <div className=" relative z-10 flex items-center justify-center"> 
           <motion.div initial={{opacity:0 , x:100}}  animate={{ opacity: 1, x: 0 }}  transition={{ duration: 0.5 ,delay:0.4}} >
-            <Image src={`/content-bg.png`} width={423} height={499} className="w-[423px] h-[499px] " alt="containerIMG"></Image>
+            <Image src={`/container.png`} width={423} height={499} className="w-[423px] h-[499px] " alt="containerIMG"></Image>
         </motion.div>
         <motion.div 
     initial={{ opacity: 0, scale: -0.5 }}
@@ -51,7 +51,7 @@
     }}
   >
     <Image 
-      src={`/oyunbatnara.png`} 
+      src={`/naraoyunbat.png`} 
       width={240} 
       height={240} 
       className="w-[240px] h-[240px] absolute -top-20 -left-10" 
@@ -67,10 +67,10 @@
     }}
   >
     <Image 
-      src={`/chingunari.png`} 
+      src={`/aricin.png`} 
       width={223} 
       height={223} 
-      className="w-[223px] h-[223px] absolute top-30 -right-30" 
+      className="w-[223px] h-[223px] absolute top-24 -right-20" 
       alt="containerIMG"
       priority 
     />
@@ -83,7 +83,7 @@
     }}
   >
     <Image 
-      src={`/jamyaoyunbat.png`} 
+      src={`/oyujam.png`} 
       width={180} 
       height={180} 
       className="w-[180px] h-[180px] absolute bottom-30 left-4" 
@@ -99,7 +99,7 @@
     }}
   >
     <Image 
-      src={`/jamyaari.png`} 
+      src={`/arijam.png`} 
       width={220} 
       height={220} 
       className="w-[220px] h-[220px] absolute -bottom-16 right-8" 
